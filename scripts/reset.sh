1365ef323da3dd2e4045a9cead7e929f3c62ee19
Lawrence <lawrence.goldstien@madetech.com>
2020-02-17 07:54:40 +0000

    refactor reset should also rebuild

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,13 +2,9 @@
 
 source scripts/_functions.sh
 
-if [[ -n $(confirm "Do you want to reset and configure the dev environment?") ]]; then
-  # Ensure images are built
-  docker-compose down
-  docker-compose rm -f
-  docker-compose build
-  docker-compose up -d
+bash "$DIR"/rebuild.sh
 
+if [[ -n $(confirm "Do you want to reset and configure the dev environment?") ]]; then
   echo "Waiting for postgres to be up and running."
   sleep 10
 
