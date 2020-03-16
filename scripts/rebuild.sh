98735d0fdf9ec93a8c30e1819bf9c71f341021c6
Lawrence <lawrence.goldstien@madetech.com>
2020-03-16 10:58:02 +0000

    when rebuilding make sure that data dirs are removed

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -6,6 +6,11 @@ if [[ -n $(confirm "Do you want to rebuild the dev environment?") ]]; then
   # Ensure images are built
   docker-compose down
   docker-compose rm -f
+
+  rm -rf ./data/auth-server
+  rm -rf ./data/feature-flag
+  rm -rf ./data/register-api
+
   docker-compose build
   docker-compose up -d
 fi
