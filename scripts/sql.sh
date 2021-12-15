d94bfcb1b8aa7d6e37e63350e155e1461113b81a
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2021-12-15 17:13:25 +0000

    remove redundant frontend build actions after using 'make sql'

--- scripts/sql.sh
+++ scripts/sql.sh
@@ -5,4 +5,3 @@ DOCKER_SERVICE_NAME=$APP
 docker-compose \
 exec "$DOCKER_SERVICE_NAME-db" \
 bash -c "psql --username epb -d epb"
- make stop && cd ../epb-frontend && make frontend-build && cd - && make start
