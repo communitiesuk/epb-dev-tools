0df3b7bed09c816b9321d564c84b1e450798336c
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2022-07-20 17:20:07 +0100

    Update Docker container names
    The latest version of Docker (or perhaps Docker Desktop) uses dashes instead of underscores as path separators in running container names.
    
    The "wait for DB to be up" step was failing because the script expected to find a container name ending in "_1" but now it instead ends in "-1".

--- scripts/security_scan.sh
+++ scripts/security_scan.sh
@@ -8,7 +8,7 @@ LOCAL_TESTING_API_SPEC=$(echo "$OPEN_API_SPEC_JSON" | jq '.servers = [{"url":"ht
 
 echo "$LOCAL_TESTING_API_SPEC" > "$DIR/../http_files/api-spec.json"
 
-PROXY_SERVER_IP=$(docker inspect epb-dev-tools_epb-proxy_1 | jq -r '.[0].NetworkSettings.Networks["epb-dev-tools_default"].IPAddress')
+PROXY_SERVER_IP=$(docker inspect epb-dev-tools_epb-proxy-1 | jq -r '.[0].NetworkSettings.Networks["epb-dev-tools_default"].IPAddress')
 
 SCAN_DATE=$(date +%s)
 
